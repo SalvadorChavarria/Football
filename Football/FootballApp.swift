65dfecedf1601568cb7c186557d9219965f7bcb7
Salvador Chavarria <chavitasoccer14@gmail.com>
2025-04-03 12:09:44 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Student2 on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
